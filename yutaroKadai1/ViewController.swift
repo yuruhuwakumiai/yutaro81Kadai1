988dd4ba636e1eace7ca5564cc4d5b4304b99df2
yuruhuwakumiai <yuruhuwakumiai@gmail.com>
2021-08-20 14:27:55 +0900

    first commit

---
+++ yutaroKadai1/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  yutaroKadai1
+//
+//  Created by 雄太郎 on 2021/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func pressButton(_ sender: Any) {
+      
+    let number1 = (textField1.text! as NSString).intValue
+    let number2 = (textField2.text! as NSString).intValue
+    let number3 = (textField3.text! as NSString).intValue
+    let number4 = (textField4.text! as NSString).intValue
+    let number5 = (textField5.text! as NSString).intValue
+    
+        let result = number1 + number2 + number3 + number4 + number5
+        
+        label.text = "\(result)"
+        
+    }
+    
+    
+    
+    
+
+}
+
